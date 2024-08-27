a50f2486d25b32d72b7c246ced88917c0323966d
SenaKanetaka <177509930+Jungkoon@users.noreply.github.com>
2024-08-27 15:26:42 +0900

    課題提出

---
+++ src/FizzBuzz.java
@@ -0,0 +1,21 @@
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+	    for (int num = 1; num <= 100; num++) {
+	        if (0 == num % 3 && 0 == num % 5) {
+	        	System.out.println("FizzBuzz");
+	        	continue;
+	        } else if (0 == num % 3) {
+	    		System.out.println("Fizz");
+	    		continue;
+	    	} else if (0 == num % 5) {
+	    		System.out.println("Buzz");
+	    		continue;
+	    	} else {
+	    	System.out.println( num );	    
+	        }
+	        }
+	}
+
+}
