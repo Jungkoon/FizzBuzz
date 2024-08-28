4799bd7efb81f2c0617e83a33d41e577f69351ea
SenaKanetaka <177509930+Jungkoon@users.noreply.github.com>
2024-08-28 20:55:47 +0900

    課題再提出

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,17 +2,16 @@
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-	    for (int num = 1; num <= 100; num++) {
-	        if (0 == num % 3 && 0 == num % 5) {
-	        	System.out.println("FizzBuzz");
-	        } else if (0 == num % 3) {
-	    		System.out.println("Fizz");
-	    	} else if (0 == num % 5) {
-	    		System.out.println("Buzz");
-	    	} else {
-	    	    System.out.println( num );	    
-	        }
-	        }
+		for (int num = 1; num <= 100; num++) {
+			if (0 == num % 3 && 0 == num % 5) {
+				System.out.println("FizzBuzz");
+			} else if (0 == num % 3) {
+				System.out.println("Fizz");
+			} else if (0 == num % 5) {
+				System.out.println("Buzz");
+			} else {
+				System.out.println( num );
+			}
+		}
 	}
 }
-	
